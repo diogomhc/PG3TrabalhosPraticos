42ffefb67ef9e624ef3b8a56e3f524f10132f149
MaheuTaroo <pantupa@gmail.com>
2022-09-22 21:23:29 +0100

    Reupload do 2º Grupo

---
+++ src/trab1/grupo2/Athlete.java
@@ -0,0 +1,7 @@
+package trab1.grupo2;
+
+public interface Athlete {
+    public String getName();
+    public String toString();
+    public boolean equals(Object o);
+}
---
+++ src/trab1/grupo2/AthleteTest.java
@@ -0,0 +1,20 @@
+package trab1.grupo2;
+
+public class AthleteTest implements Athlete {
+    private final String name;
+
+    public AthleteTest(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String toString() {
+        return getName();
+    }
+    public boolean equals(Object o){
+        return o instanceof AthleteTest &&
+               getName().equals(((AthleteTest)o).getName());
+    }
+}
